5118c486d98320d854a40b5f9d542714168b6e50
Xavier Lavayssière <xavier.lavayssiere@gmail.com>
2019-05-23 14:43:31 +0200

    Scene Ouverte

---
+++ SceneOuverte/data.js
@@ -0,0 +1,73 @@
+let contractAddress = "0xd7beb8308e343c4d40cc9c05c03690f79341a557"
+let contractABI =[
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "creneauxLibres",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "artisteEnCours",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "passerArtisteSuivant",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "nomDArtiste",
+				"type": "string"
+			}
+		],
+		"name": "sInscrire",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "passagesArtistes",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
